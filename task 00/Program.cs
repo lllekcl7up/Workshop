b5297d1c62fd747ef9d1744229e85ae67c388e1d
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-11 16:08:08 +0300

    Принимает число и выдает его квадрат

---
+++ task 00/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine()); // Конвертировали введенную строку в число
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+
+
+
