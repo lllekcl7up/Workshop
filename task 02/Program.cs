f27204eb137b13807c6566b870606228705b267b
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-11 17:34:22 +0300

    Задача 2 (Выдает какое число больше какое меньше)

---
+++ task 02/Program.cs
@@ -0,0 +1,9 @@
+﻿// // Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+    if (numberA > numberB) Console.WriteLine("Первое число больше второго");
+    else Console.WriteLine("Второе число больше первого");
+
