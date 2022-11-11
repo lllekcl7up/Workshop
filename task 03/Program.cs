eedbf1810e270940107329ad0d750c0e54a0f4be
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-11 16:24:06 +0300

    На входе принимает число N, на выходе все целые число от -N до N

--- task 03/Program.cs
+++ task 03/Program.cs
@@ -3,11 +3,11 @@
 Console.WriteLine("Введите номер дня недели: ");
 int numDay = Convert.ToInt32(Console.ReadLine());
 
-if(numDay == 1)Console.WriteLine("Понедельник");
-else if(numDay == 2) Console.WriteLine("Вторник");
-else if(numDay == 3) Console.WriteLine("Среда");
-else if(numDay == 4) Console.WriteLine("Четверг");
-else if(numDay == 5) Console.WriteLine("Пятница");
-else if(numDay == 6) Console.WriteLine("Суббота");
-else if(numDay == 7) Console.WriteLine("Воскресенье");
+if(numDay == 1)Console.WriteLine("Первый день недели это Понедельник");
+else if(numDay == 2) Console.WriteLine("Второй день недели это Вторник");
+else if(numDay == 3) Console.WriteLine("Третий день недели это Среда");
+else if(numDay == 4) Console.WriteLine("Четвертый день недели это Четверг");
+else if(numDay == 5) Console.WriteLine("Пятый день недели это Пятница");
+else if(numDay == 6) Console.WriteLine("Шестой день недели это Суббота");
+else if(numDay == 7) Console.WriteLine("Седьмой день недели это Воскресенье");
 else Console.WriteLine("Такого дня недели нет");
