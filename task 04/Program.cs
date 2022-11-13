d0a591cf3f21e8206ef3c87ca5f14d5077e20237
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-13 15:27:21 +0300

    Задача 4 (Принимает на вход 3 числа и выдает макс)

---
+++ task 04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+    if (numberB > max) max = numberB;
+    if (numberC > max) max = numberC;
+Console.WriteLine($"Максимальное из трёх введеных чисел: {max} ");
