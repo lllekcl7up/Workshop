23fb176b0d42c97d1455d4ca123224e1bf3f4216
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-13 16:00:26 +0300

    Проверяет является ли число чётным или нет

---
+++ task 06/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, является ли число чётным(делится ли оно на 2 без остатка)
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+    if (numberA % 2 == 0 )Console.WriteLine($" {numberA} - является чётным числом");
+else Console.WriteLine($" {numberA} - не является четным числом");
