8ef033a332caec404d17ce7389822b04e6ef1845
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-15 21:03:19 +0300

    Является ли одно число квадратом другого

---
+++ task 16/Program.cs
@@ -0,0 +1,20 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Square(numberA,numberB) ? "Является" : "Не является");
+
+bool Square( int firstNumber, int secondNumber)
+{
+    return (firstNumber == secondNumber*secondNumber) || (secondNumber == firstNumber*firstNumber);
+}
