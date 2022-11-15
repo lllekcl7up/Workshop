e943b2efd106347add62f347a5c19d64fcf38587
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-15 21:39:28 +0300

    Показывает вторую цыфру числа

---
+++ task 10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трёхзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int ShowSecondDigit(int num)
+{
+    int Digit = num / 10 % 10;
+    int result = Digit;
+    return result;
+}
+
+Console.WriteLine($"Вторая цифра = {ShowSecondDigit(numberA)}");
