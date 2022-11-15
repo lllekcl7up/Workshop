f213b90b4bce0a7b65efc2139d1239ccaa15bd09
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-15 20:40:25 +0300

    Выводит случайное 3х значное число и удаляет 2ю цифру

---
+++ task 11/Program.cs
@@ -0,0 +1,31 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(99, 1000);
+Console.WriteLine($"Случайное число из отрезка 99 - 1000 --> {number}");
+
+
+//    int firstDigit = number / 100;  
+//    int secondDigit = number % 10;
+//   Console.WriteLine($"Число:  {firstDigit}{secondDigit} ");
+
+// int res = firstDigit * 10 + secondDigit;
+// Console.WriteLine(res);
+
+int RemoveSecondDigit(int num) // int num = number
+{
+    int firstDigit = num / 100;  
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+
+
+// int maxDigit = MaxDigit(number);
+Console.WriteLine("Удаляем вторую цифру");
+Console.WriteLine($"Результат = {RemoveSecondDigit(number)}");
