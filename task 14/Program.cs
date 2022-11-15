1d0bad7e61e660adbe8967f808f14c8184fff889
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-15 20:57:13 +0300

    Проверка числа кратно ли 7 и 23

---
+++ task 14/Program.cs
@@ -0,0 +1,27 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+
+Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+bool Result(int numberA)
+{
+    return numberA % 7 == 0 && numberA % 23 == 0;
+}
+
+Console.WriteLine( Result(numberA) ? "Кратно 7 и 23" : "Не кратно 7 и 23");
+
+
+
+//простое решение с if
+// if (numberA % 7 == 0 && numberA % 23 == 0)
+// {
+//     Console.WriteLine("Кратно 7 и 23: ");
+// }
+// else Console.WriteLine("Не кратно 7 и 23: ");
