9deddf4146598706b375904f1cb45009a8bdd732
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-16 11:43:57 +0300

    Показывает третье число

---
+++ task 13/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает что третьей цифры нет.
+
+Console.WriteLine("Введите трёхзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int ShowThirdDigit(int num)
+{
+    int result = 0;
+        if (num >= 100)
+        {
+            while (num > 999)
+            {
+            num = num / 10;
+            }
+            result = num % 10;
+        }
+    return result;
+}
+if (ShowThirdDigit(numberA) == 0)
+{
+Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+Console.WriteLine($"Третья цифра: {ShowThirdDigit(numberA)}");
+}
