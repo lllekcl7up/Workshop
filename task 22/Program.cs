5e52bad7af5a205564e8c7ef1f216f1c4a03b0ce
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-18 22:10:04 +0300

    Таблица от 1 до N

--- task 22/Program.cs
+++ task 22/Program.cs
@@ -1,14 +1,37 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдает
 // таблицу квадратов чисел от 1 до N.
 
-Console.WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-void Square(int num)
+// void Square(int num)
+// {
+//     for (int i = 1; i <= num; i++)
+// {
+//         Console.WriteLine($"{i} -> {i * i} ");
+// }
+// }
+// Square(number);
+
+
+int n = -1; // Сделали переменную меньше 1 
+while (n < 1) // до тех пор пока N не будет больше одного
 {
-    for (int i = 1; i <= num; i++)
+    Console.WriteLine("Введите положительное число");
+    n = Convert.ToInt32(Console.ReadLine());
+    if ( n < 1 ) Console.WriteLine("Вы ввели неверное число");
+}
+
+string SqrTable(int num)
 {
-        Console.WriteLine($"{i} -> {i * i} ");
+    int count = 1;
+    string table = "";
+    while (count <= num)
+    {
+        table = table + $"{count}|{count * count} \n";
+        count ++;
+    }
+    return table;
 }
-}
-Square(number);
+string sqrTable = SqrTable(n);
+Console.WriteLine(sqrTable);
