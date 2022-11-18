0c142de955c1651fec6d0118cda41bd6af93827b
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-18 17:24:53 +0300

    Диапазон координат по номеру четверти

--- task 18/Program.cs
+++ task 18/Program.cs
@@ -2,15 +2,15 @@
 // диапазон возможных координат точек в этой четверти (x и y).
 
 Console.WriteLine("Введите номер четверти:");
-int quarter = Convert.ToInt32(Console.ReadLine());
+string quarter = Console.ReadLine();
 
-string Range (int quarter1)
+string Range (int quarter1) // тут мы через строку определяем а не через математические вычисления
 {
-    if (quarter1 == 1) return "x > 0 и y > 0";
-    if (quarter1 == 2) return "x < 0 и y > 0";
-    if (quarter1 == 3) return "x < 0 и y < 0";
-    if (quarter1 == 4) return "x > 0 и y < 0";
-    return "Введен неверный номер четверти";
+    if (quarter1 == "1") return "x > 0 и y > 0";
+    if (quarter1 == "2") return "x < 0 и y > 0";
+    if (quarter1 == "3") return "x < 0 и y < 0";
+    if (quarter1 == "4") return "x > 0 и y < 0";
+    return "Введен неверный данные";
 }
 string range = Range(quarter);
 
