4e37d12e341a0d5a230d22f42fe14cd319a52d1d
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-19 16:23:47 +0300

    Проверка 5ти значного числа палиндром ли

---
+++ task 19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите прогрмамму, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+
+// Я пытался вытащить метод из общей конструкции но у меня так 
+// и не получилось. Решение большое и громоздкое но я для своего
+// понимания расписывал работу строк что б получше понять как они
+// работают. Без метода с циклом решение может быть компактнее.
+
+    Console.Write("Введите пятизначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+if ( number > 10000 && number < 100000 )
+{
+void PolindromShow ( int num )
+{
+int firstDigit = num / 10000;
+int fiveDigit = num % 10;
+int secondDigit = num / 1000 % 10;
+int fourthDigit = num / 10 % 10;
+    if (firstDigit == fiveDigit && secondDigit == fourthDigit)
+    {
+        Console.WriteLine($"Число {number} является палиндром");
+    }
+else Console.WriteLine($"Число {number} не является палиндромом");
+}
+PolindromShow(number);
+}
+else  Console.Write("Вы ввели не пятизначное число");
