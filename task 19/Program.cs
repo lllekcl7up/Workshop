780214e33cc09b3de59a98c34e7def3ffa29d3ea
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-21 17:17:41 +0300

    Проверка 5ти значного числа палиндром ли

--- task 19/Program.cs
+++ task 19/Program.cs
@@ -6,6 +6,8 @@
 // понимания расписывал работу строк что б получше понять как они
 // работают. Без метода с циклом решение может быть компактнее.
 
+// 1 решение 
+
 //     Console.Write("Введите пятизначное число: ");
 //     int number = Convert.ToInt32(Console.ReadLine());
 // if ( number > 10000 && number < 100000 )
@@ -25,6 +27,8 @@
 // PolindromShow(number);
 // }
 // else  Console.Write("Вы ввели не пятизначное число");
+
+// 2 решение
 
 bool PolindromShow ( int num )
 {
@@ -50,3 +54,22 @@ else
 {
   Console.Write("Вы ввели не пятизначное число");
 }
+
+// 3 решение (Решение группы)
+
+// Console.WriteLine("Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int Reverse( int num)
+// {
+//     int result = 0;
+//     while ( num != 0)// пока num не будет равно нулю выполняем следующий цикл
+//     {
+//         int ostatok;
+//         num = Math.DivRem(num, 10, out ostatok);
+//         result = result * 10 + ostatok;
+//     }
+//     return result;
+// }
+// int a = Reverse(number);
+// Console.WriteLine(( a == number) ? "Yes it is Polindrom" : "No it is not");
