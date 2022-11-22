40330a2dcf094b8a295fbb2864749ea1f7b9397d
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-22 15:07:51 +0300

    Выдает кол-во цифр в числе

---
+++ task 26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Digits(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count == 0 ? 1 : count;
+}
+if (number < 0) number = -number;
+int result = Digits(number);
+Console.WriteLine($"Колличество цифр в числе {number} = {result}");
+
+// Math.Abs(-123)
