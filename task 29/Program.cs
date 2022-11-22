b38c543498d85d4d73a730fe24801c11ba080b3e
Dima Skorobogatov <lllekcl7up@gmail.com>
2022-11-22 22:13:02 +0300

    Рандомный массив на экран

---
+++ task 29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задает массив из 8 элементов заполненый псевдослучайными числами 
+// и выводит из на экран . 
+
+int [] array = new int[8];
+
+void RandomArray (int [] arrayA)
+{
+    Random rndm = new Random();
+    for ( int i = 0; i < arrayA.Length; i++)
+    {
+        arrayA[i] = rndm.Next(0, 999);
+    }
+}
+
+void PrintArray(int[] arrayB)
+{
+    for (int i = 0; i < arrayB.Length; i++)
+{
+    Console.Write($" {arrayB[i]} ");
+}
+}
+
+RandomArray(array);
+PrintArray(array);
