0d76f60d5266ff7dc600ddef88dac478656ba2ed
Dima Skorobogatov <lllekcl7up@gmail.com>
2022-11-22 21:42:26 +0300

    Сумма цифр в числа

---
+++ task 27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+// 452 -> 11 
+// 82 -> 10
+
+int SumDigits( int num )
+{
+    int sum = default; 
+    if (num < 0 )
+    num = num * -1;
+    for ( int i = 0; num > 0; i++)
+    {
+        sum = sum + num % 10;
+        num =num / 10;
+    }
+    return sum;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр числа {number} = {SumDigits(number)}");
