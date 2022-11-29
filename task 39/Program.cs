bf6a2e692da79632251ab06651684055df99c4db
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-11-29 15:37:18 +0300

    Реверс Массива

---
+++ task 39/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] CreateArrayRandomInt (int size, int min, int max)
+{
+    int [] array = new int [size];
+    Random rndm = new Random();
+    
+    for(int index = 0; index < array.Length; index = index + 1)
+    {
+        array[index] = rndm.Next( min, max+1 );// с учетом последнего индекса поэтому + 1
+    }
+    return array;
+}
+
+void PrintArray(int[] arr, string symb1, string symb2)
+{
+    Console.Write(symb1);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write(symb2);
+}
+
+void ReverseArray (int[] array) // метод разварота массива
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length -1 - i];
+        array[array.Length -1 - i] = temp;
+    }
+}
+
+int [] arrayCreate = CreateArrayRandomInt(5, 1, 9);
+PrintArray(arrayCreate, "[", "]");
+ReverseArray(arrayCreate);
+Console.WriteLine();
+PrintArray(arrayCreate, "[", "]");
+
