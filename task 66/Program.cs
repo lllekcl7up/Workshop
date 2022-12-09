b21d7fb2a9aaa6e835241fed64f3024f09c074b9
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-12-09 21:43:35 +0300

    Сумма чисел от M до N

---
+++ task 66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в 
+// промежутке от M до N. Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumFromMtoN(int m, int n)
+{
+    if (n == m) return n;
+    else if (n > m) return m + SumFromMtoN(n, m + 1);
+    else return m + SumFromMtoN(n, m - 1);
+}
+int sum = SumFromMtoN(numberM, numberN);
+Console.WriteLine($"Сумма чисел от M до N = {sum}");
+
+
+
+
+
+
+
