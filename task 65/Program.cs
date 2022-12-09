6c80fbfdb0f3503241c083f584ef2cf92414a542
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-12-09 13:16:19 +0300

    Рекурсия выдает числа от M до N

---
+++ task 65/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+
+
+Console.WriteLine("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+NumbersMToN(numberM,numberN);
+
+void NumbersMToN(int numbM, int numbN)
+{
+    if (numbM > numbN)
+    {
+        Console.Write($"{numbM} ");
+        NumbersMToN(numbM - 1, numbN);
+    }
+    else if (numbM < numbN)
+    {
+        Console.Write($"{numbM} ");
+        NumbersMToN(numbM + 1, numbN);
+    }
+    else
+    {
+        Console.Write($"{numbM} ");
+    }
+}
