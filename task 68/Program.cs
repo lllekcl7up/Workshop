e0f394aa8a40c73ebb20ef6d8fd86a22771719a0
lllekcl7up <115775742+lllekcl7up@users.noreply.github.com>
2022-12-11 09:12:09 +0300

    Функция Аккермана

--- task 68/Program.cs
+++ task 68/Program.cs
@@ -11,9 +11,9 @@ int numberN = Convert.ToInt32(Console.ReadLine());
 int Akkerman(int m, int n)
 {
     if (m == 0) return n + 1;
-    else if (m != 0 && n == 0) return Akkerman(m - 1, 1);
-    else if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
-    else return Akkerman(m, n);
+    if (m != 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(m, n);
 }
 int akk = Akkerman(numberM,numberN);
 Console.WriteLine($"Для чисел m = {numberM} и n = {numberN}\nФункция Аккермана = {akk}");
